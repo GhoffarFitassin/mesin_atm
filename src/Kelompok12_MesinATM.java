5a6d370f4112c49f5462afe53a17374f3589fdc6
ghoffar8804@gmail.com <ghoffar8804@gmail.com>
2023-09-18 13:27:56 +0700

    Tarik Saldo

--- src/Kelompok12_MesinATM.java
+++ src/Kelompok12_MesinATM.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class Kelompok12_MesinATM {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double saldo, tarik;
+        Scanner input = new Scanner(System.in);
+        System.out.println("isi saldo");
+        System.out.print("Isi nominal : ");
+        saldo = input.nextDouble();
+        System.out.println("isi uang yang ingin ditarik");
+        System.out.print("Isi nominal : ");
+        tarik = input.nextDouble();
+        saldo -= tarik;
+        System.out.println("sisa saldo : " + saldo);
+        System.out.println("jumlah uang yang ditarik : " + tarik);
     }
 }
