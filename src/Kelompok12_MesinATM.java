93411c0dfa345cf57dba81937e5da4b621ffce0f
ghoffar8804@gmail.com <ghoffar8804@gmail.com>
2023-10-06 08:11:58 +0700

    include system to login

--- src/Kelompok12_MesinATM.java
+++ src/Kelompok12_MesinATM.java
@@ -11,16 +11,16 @@ public class Kelompok12_MesinATM {
 
         System.out.println("========================================================================================");
         System.out.println("\t\r\n" + //
-        "\t███╗   ███╗███████╗███████╗██╗███╗   ██╗     █████╗ ████████╗███╗   ███╗\r\n" + //
-        "\t████╗ ████║██╔════╝██╔════╝██║████╗  ██║    ██╔══██╗╚══██╔══╝████╗ ████║\r\n" + //
-        "\t██╔████╔██║█████╗  ███████╗██║██╔██╗ ██║    ███████║   ██║   ██╔████╔██║\r\n" + //
-        "\t██║╚██╔╝██║██╔══╝  ╚════██║██║██║╚██╗██║    ██╔══██║   ██║   ██║╚██╔╝██║\r\n" + //
-        "\t██║ ╚═╝ ██║███████╗███████║██║██║ ╚████║    ██║  ██║   ██║   ██║ ╚═╝ ██║\r\n" + //
-        "\t╚═╝     ╚═╝╚══════╝╚══════╝╚═╝╚═╝  ╚═══╝    ╚═╝  ╚═╝   ╚═╝   ╚═╝     ╚═╝\r\n" + //
-        "\t                                                                        \r\n" + //
-        "");
+                "\t███╗   ███╗███████╗███████╗██╗███╗   ██╗     █████╗ ████████╗███╗   ███╗\r\n" + //
+                "\t████╗ ████║██╔════╝██╔════╝██║████╗  ██║    ██╔══██╗╚══██╔══╝████╗ ████║\r\n" + //
+                "\t██╔████╔██║█████╗  ███████╗██║██╔██╗ ██║    ███████║   ██║   ██╔████╔██║\r\n" + //
+                "\t██║╚██╔╝██║██╔══╝  ╚════██║██║██║╚██╗██║    ██╔══██║   ██║   ██║╚██╔╝██║\r\n" + //
+                "\t██║ ╚═╝ ██║███████╗███████║██║██║ ╚████║    ██║  ██║   ██║   ██║ ╚═╝ ██║\r\n" + //
+                "\t╚═╝     ╚═╝╚══════╝╚══════╝╚═╝╚═╝  ╚═══╝    ╚═╝  ╚═╝   ╚═╝   ╚═╝     ╚═╝\r\n" + //
+                "\t                                                                        \r\n" + //
+                "");
         System.out.println("========================================================================================");
-        
+
         System.out.println("========== Login ==========");
         System.out.print("Masukkkan username: ");
         user = input.nextLine();
@@ -31,72 +31,71 @@ public class Kelompok12_MesinATM {
         if ((user.equals(username))) {
             if ((pw.equals(password))) {
                 System.out.println("Login Berhasil");
+                System.out.println("");
+                System.out.println("********** Bank Suka Maju **********");
+                System.out.println("-----------------------------------");
+                System.out.println("\n1.Cek Saldo");
+                System.out.println("2.Simpan Uang");
+                System.out.println("3.Ambil Uang");
+                System.out.println("4.History Transaksi");
+                System.out.println("5.Help");
+                System.out.println("6.keluar");
+                System.out.print("\nPilih Menu : ");
+                pilihan = input.nextInt();
+                switch (pilihan) {
+                    case 1:
+                        System.out.println("=======================================");
+                        System.out.println("              Cek Saldo");
+                        System.out.println("=======================================");
+                        System.out.println("\n     Saldo Anda Saat Ini : " + saldo);
+                        System.out.println("\n=======================================");
+                        break;
+                    case 2:
+                        System.out.println("======================================");
+                        System.out.println("              Setor Tunai");
+                        System.out.println("======================================");
+                        System.out.print("\nNominal Yang Ingin Anda Setor: ");
+                        tambah = input.nextDouble();
+                        saldo += tambah;
+                        System.out.println("Jumlah Uang Yang Di Setor    : " + tambah);
+                        System.out.println("Saldo Saat Ini               : " + saldo);
+                        System.out.println("\n======================================");
+                        break;
+                    case 3:
+                        System.out.println("isi uang yang ingin ditarik");
+                        System.out.print("Isi nominal : ");
+                        tarik = input.nextDouble();
+                        saldo -= tarik;
+                        System.out.println("sisa saldo : " + saldo);
+                        System.out.println("jumlah uang yang ditarik : " + tarik);
+                        break;
+                    case 4:
+                        break;
+                    case 5:
+                        System.out.println("=================================================");
+                        System.out.println("                      Help                       ");
+                        System.out.println("1. Gunakan Menu ke-1 jika anda ingin melihat saldo anda");
+                        System.out.println("2. Gunakan Menu ke-2 jika anda ingin mengisi saldo");
+                        System.out.println("3. Gunakan Menu ke-3 jika anda ingin melakukan penarikan saldo");
+                        System.out.println("4. Gunakan Menu ke-4 jika anda ingin melihat history transaksi");
+                        System.out.println("6. Gunakan Menu ke-6 jika anda selesai menggunakan mesin ATM");
+                        // System.out.println("7. Saat penarikan, saldo minimal anda adalah Rp.500000");
+                        // System.out.println("8. Jika terdapat masalah pada mesin ATM hubungi
+                        // 021-5437xxx");
+                        System.out.println("=================================================");
+                        break;
+                    case 6:
+                        System.exit(0);
+                        break;
+                    default:
+                        System.exit(0);
+                }
+                input.close();
             } else {
                 System.out.println("Password salah");
             }
         } else {
             System.out.println("Username salah");
         }
-
-
-        System.out.println("");
-        System.out.println("********** Bank Suka Maju **********");
-        System.out.println("-----------------------------------");
-        System.out.println("\n1.Cek Saldo");
-        System.out.println("2.Simpan Uang");
-        System.out.println("3.Ambil Uang");
-        System.out.println("4.History Transaksi");
-        System.out.println("5.Help");
-        System.out.println("6.keluar");
-        System.out.print("\nPilih Menu : ");
-        pilihan = input.nextInt();
-        switch (pilihan) {
-            case 1:
-            System.out.println("=======================================");
-            System.out.println("              Cek Saldo");
-            System.out.println("=======================================");
-            System.out.println("\n     Saldo Anda Saat Ini : " + saldo);
-            System.out.println("\n=======================================");
-            break;
-            case 2:
-            System.out.println("======================================");
-            System.out.println("              Setor Tunai");
-            System.out.println("======================================");
-            System.out.print("\nNominal Yang Ingin Anda Setor: ");
-            tambah = input.nextDouble();
-            saldo += tambah;
-            System.out.println("Jumlah Uang Yang Di Setor    : " + tambah);
-            System.out.println("Saldo Saat Ini               : " + saldo);
-            System.out.println("\n======================================");
-            break;
-            case 3:
-            System.out.println("isi uang yang ingin ditarik");
-            System.out.print("Isi nominal : ");
-            tarik = input.nextDouble();
-            saldo -= tarik;
-            System.out.println("sisa saldo : " + saldo);
-            System.out.println("jumlah uang yang ditarik : " + tarik);
-            break;
-            case 4:
-            break;
-            case 5:
-                System.out.println("=================================================");
-                System.out.println("                      Help                       ");
-                System.out.println("1. Gunakan Menu ke-1 jika anda ingin melihat saldo anda");
-                System.out.println("2. Gunakan Menu ke-2 jika anda ingin mengisi saldo");
-                System.out.println("3. Gunakan Menu ke-3 jika anda ingin melakukan penarikan saldo");
-                System.out.println("4. Gunakan Menu ke-4 jika anda ingin melihat history transaksi");
-                System.out.println("6. Gunakan Menu ke-6 jika anda selesai menggunakan mesin ATM");
-                // System.out.println("7. Saat penarikan, saldo minimal anda adalah Rp.500000");
-                // System.out.println("8. Jika terdapat masalah pada mesin ATM hubungi 021-5437xxx");
-                System.out.println("=================================================");
-                break;
-            case 6:
-            System.exit(0);
-            break;
-            default:
-            System.exit(0);
-        }
-        input.close();
     }
 }
